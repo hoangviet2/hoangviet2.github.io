87e88cef937258e0e0b4f39b1063f541fa92e443
hoangviet2 <hoangviet18306@gmail.com>
2022-06-17 22:04:19 +0700

    abc

--- js/myJs.js
+++ js/myJs.js
@@ -4,7 +4,7 @@ const textConfig = {
   text3: "Hi, nói sao nhỉ có lẽ mình biết cậu từ đầu năm lớp 10 òi tại mình thấy bạn là bạn của TMin và bạn cũng rất xinh nhưng mà lúc đó mình cũng chỉ vừa mới có cảm tình với bạn hoi. Rồi năm lớp 10 qua đi mình cũng bận bịu với việc học rồi thi tuyển hsg này nọ nhưng mà tớ vẫn theo dõi cậu và tìm hiểu thêm về cậu. Sau khi nói chiện với cậu thì mình thấy mình thực sự thích cậu 🥰.",
   text4: "Cậu có thể trở thành bạn gái mình hong 🥺 Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
   text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
+  text6: "Yêu ơi là yêu ❤️",
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
   text9: "Vì cậu đẹp try vlllll",
